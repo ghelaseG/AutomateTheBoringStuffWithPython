6e024265b2c187431f46056ba3cb0c2f844126a0
ghelaseG <96828940+ghelaseG@users.noreply.github.com>
2022-04-12 22:38:01 +0100

    Update DownloadingGoogleFormsData.py

--- Chapter 14/DownloadingGoogleFormsData.py
+++ Chapter 14/DownloadingGoogleFormsData.py
@@ -6,5 +6,5 @@ import ezsheets #install ezsheets accordingly to this website: https://ezsheets.
 from pprint import pprint #use it just to make it "prettier"
 sheet = ezsheets.Spreadsheet('17mvaA1tHF7Rhtho9wc-dibRIJ-qSD_O8vbut_cnpF3A') #The unique ID for a Google Sheets spreadsheet can be found in the URL, after the spreadsheets/d/ part and before the /edit part
 s = sheet.sheets[0] #Get the first sheet in this spreadsheet
-ss = s.getColumn('C') #instead of 3 you can use s.getColumn('C') - will be the same
+ss = s.getColumn(3) #instead of 3 you can use s.getColumn('C') - will be the same
 pprint(ss)
